04b9d99c1153e18844ca0138b742ad450359461e
kuomolew <kuomolew@gmail.com>
2019-11-08 23:23:55 +0200

    08

--- js/08-days.js
+++ js/08-days.js
@@ -1,8 +1,11 @@
 //Зациклить вывод дней недели таким образом: "День недели. Хотите увидеть следующий день?"
 
 
-document.getElementById('counter').insertAdjacentHTML("afterbegin", '<h1>Result: <span id="result"></span></h1>');
-
+document.getElementById('counter').insertAdjacentHTML("afterbegin", '<h1><span id="day"></span></h1>');
+let week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let i = 0;
 let calculate = function() {
-    let week = [];
+    document.getElementById('day').innerText = week[i];
+    i++;
+    i = i % week.length;
 }
